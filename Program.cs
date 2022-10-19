b84b706b6133b16c80f56486891ea0f958390914
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-19 20:47:09 +0530

    checkdayofweek

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace dayofweek
+{
+    class program
+    {
+        
+        static void Main(string[] args)
+        {
+            int weekday;
+
+         
+            Console.WriteLine("enter weekday number(0-6)");
+            weekday = Convert.ToInt32(Console.ReadLine());
+
+        
+            switch (weekday)
+            {
+                case 0:
+                    Console.WriteLine("it is sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("it is monday");
+                    break;
+                case 2:
+                    Console.WriteLine("it is tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("it is wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("it is thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("it is friday");
+                    break;
+                case 6:
+                    Console.WriteLine("it is saturday");
+                    break;
+
+            }
+
+
+        }
+    }
+}
